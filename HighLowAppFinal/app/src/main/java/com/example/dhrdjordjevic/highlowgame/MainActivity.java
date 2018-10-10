3579ccdd3ea87b18025044ffdce765709bc3dd50
Stefan Djordjevic <stefan_djordjevic1991@hotmail.com>
2018-10-10 11:04:20 +0200

    Fixed some issues with scoring

--- HighLowAppFinal/app/src/main/java/com/example/dhrdjordjevic/highlowgame/MainActivity.java
+++ HighLowAppFinal/app/src/main/java/com/example/dhrdjordjevic/highlowgame/MainActivity.java
@@ -135,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         mScoreText.setText("SCORE: " + mScore);
+        UpdateUI();
 
         if(mScore > mHighScore)
         {
