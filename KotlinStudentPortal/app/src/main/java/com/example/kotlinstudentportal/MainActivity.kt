fa57c8550008b3af923c388ee99b4cba1a1f4c1b
Stefan Djordjevic <stefan_djordjevic1991@hotmail.com>
2019-10-14 17:46:06 +0200

    StudentPortal buttonlayout fix

--- KotlinStudentPortal/app/src/main/java/com/example/kotlinstudentportal/MainActivity.kt
+++ KotlinStudentPortal/app/src/main/java/com/example/kotlinstudentportal/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlinstudentportal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -35,11 +36,13 @@ class MainActivity : AppCompatActivity()
             val portalAdapter = PortalAdapter(portals)
             rvPortals.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
             rvPortals.adapter = portalAdapter
+            portalAdapter.notifyDataSetChanged()
         } else
         {
             val portalAdapter = PortalAdapter(portals)
             rvPortals.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
             rvPortals.adapter = portalAdapter
+            portalAdapter.notifyDataSetChanged()
         }
     }
 
